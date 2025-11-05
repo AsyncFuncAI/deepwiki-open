59abbc430e48e53d00ea77483123060a2f18f03b
stefpi <stef@ceatanostra.org>
2025-11-05 16:00:48 -0500

    apply fix to included directories as well

--- api/data_pipeline.py
+++ api/data_pipeline.py
@@ -249,7 +249,7 @@ def read_all_documents(path: str, embedder_type: str = None, is_ollama_embedder:
             # Check if file is in an included directory
             if included_dirs:
                 for included in included_dirs:
-                    clean_included = included.strip("./").rstrip("/")
+                    clean_included = included.split("./")[1].rstrip("/")
                     if clean_included in file_path_parts:
                         is_included = True
                         break
