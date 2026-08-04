33afd28745f7619ef2be7dbae327b61d4cf18313
GdoongMathew <gdoongmathew@gmail.com>
2026-08-04 11:15:48 +0800

    fix import path

--- api/routers/wiki.py
+++ api/routers/wiki.py
@@ -21,12 +21,13 @@ from api.schemas import (
 from api.services.wiki import (
     delete_wiki_cache,
     export_wiki,
+    generate_repo_wiki,
     list_processed_projects,
     list_wiki_cache,
     read_wiki_cache,
+    registry,
+    WikiTask,
 )
-
-from api.services.wiki import generate_repo_wiki, WikiTask, registry
 
 logger = get_logger(__name__)
 
